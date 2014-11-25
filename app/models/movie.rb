8eb84bbef2854bac1427a037421e8a478f62db73
tfchang <tingfchang@gmail.com>
2014-11-24 17:18:33 -0800

    Step 5: validations for Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :poster_image_url, presence: true
+  validates :release_date, presence: true
+
+  validate :release_date_in_future
+
+  private
+
+  def release_date_in_future
+    if release_date.present? && release_date < Date.today
+      errors.add(:release_date, "should probably be in the future")
+    end
+  end
 end
