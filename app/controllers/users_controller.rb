8ca84929b12cf58cbda6d02bd2c95a013b37cce2
tfchang <tingfchang@gmail.com>
2014-11-25 11:43:23 -0800

    Step 9: created sessions controller to track user logins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      # auto login
+      session[:user_id] = @user.id 
       redirect_to movies_path
     else
       render :new
