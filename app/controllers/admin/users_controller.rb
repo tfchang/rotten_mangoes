f1e4df526e8afb325b6ac3c753307e40059a290a
tfchang <tingfchang@gmail.com>
2014-11-26 11:56:24 -0800

    Enhancement #2: admin authentication

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < ApplicationController
+  
+  before_action :verify_admin
+
   def index
     @admin_users = User.all
   end
@@ -27,7 +30,20 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_path
   end
 
+
+
   private
+
+  def verify_admin
+    unless is_admin?
+      redirect_to root, alert: "You are not an administrator!"
+    end
+  end
+
+  def is_admin?
+    current_user
+    @current_user && @current_user.id == 1
+  end
 
   def user_params
     params.require(:user).permit(:firstname, :lastname, :email)
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,4 @@ class User < ActiveRecord::Base
   def full_name
     "#{firstname} #{lastname}"
   end
-
 end
