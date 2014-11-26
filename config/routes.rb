1ce0631b5d2e864928cb9e5ac63d66cc37b81232
tfchang <tingfchang@gmail.com>
2014-11-26 10:42:09 -0800

    Enhancement #2: add admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :admin do
+    resources :users
+  end
+
   root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
