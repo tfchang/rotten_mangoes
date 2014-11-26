e307c32204af5008461b524ebcf74eff2df8f28f
tfchang <tingfchang@gmail.com>
2014-11-26 11:03:53 -0800

    Enhancement #2: add admin/users#show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,12 @@ class Admin::UsersController < ApplicationController
   def index
     @admin_users = User.all
   end
+
+  def show
+    @admin_user = User.find(params[:id])
+  end
+
+  # def destroy
+  #   @admin_user = User.new
+  # end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,2 @@
 module MoviesHelper
-  def format_date(date)
-    date.strftime("%b %d, %Y")
-  end
 end
