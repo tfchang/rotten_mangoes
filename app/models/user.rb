f9482e33f753eaf3d36e3ac666a014dde5b83d3b
tfchang <tingfchang@gmail.com>
2014-11-25 17:29:50 -0800

    Step 12: add Review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :reviews
+
   validates :email, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
